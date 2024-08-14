9139de07014f1254ceac99550962590dba044e6a
André vargas <dedilu@gmail.com>
2024-08-14 20:20:51 -0300

    Update 14/08

--- app/init.py
+++ app/init.py
@@ -6,23 +6,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/novoaluno')
+@app.route('/cadastro')
 def cadastrar_aluno():
     return render_template('novoaluno.html')
 
 @app.route('/diario')
-def logar():
+def abirdiario():
     return render_template('dirariobordo.html')
 
-@app.route('/diario',methods=['post'])
-def abirdiario():
-    ra = request.form('ra')
-    if ra == 12345619:
-        return render_template('diariobordo.html')
+@app.route('/logar',methods=['POST'])
+def logar():
+    ra = request.form['ra']
+    if ra == '12345619':
+        return render_template('diariobordo.html',ra=ra)
     else:
         return f'O ra está errado'
 app.run(debug=True)
 
-
-
-#AMOALANAS2S2
