5324eac44ddb93c9f75d7210e387fcead601b55e
André vargas <dedilu@gmail.com>
2024-08-15 11:39:13 -0300

    Aula 15/08

--- app/init2.py
+++ app/init2.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 import urllib.parse
 
 user = 'root'
-password = urllib.parse.quote_plus('andre123')
+password = urllib.parse.quote_plus('senai@123')
 
 host = 'localhost'
 database = 'projetodiario1'
@@ -44,13 +44,17 @@ def cadastrar_aluno():
 def abirdiario():
     return render_template('dirariobordo.html')
 
-@app.route('/logar',methods=['POST'])
+@app.route('/logar', methods=['POST'])
 def logar():
     ra = request.form['ra']
-    if ra == '12345619':
-        return render_template('diariobordo.html',ra=ra)
+    aluno = session.query(Aluno).filter_by(ra=ra).first()
+    
+    if aluno:
+        nome = aluno.nome
+        return render_template('diariobordo.html', ra=ra, nome=nome)
     else:
-        return f'O ra está errado'
+        mensagem = "RA INVALIDA"
+        return render_template('index.html', mensagem=mensagem)
     
 
 @app.route('/criaraluno', methods=['POST'])
@@ -62,7 +66,7 @@ def criar():
     aluno = Aluno(ra=ra,nome=nome,tempoestudo=tempoestudo,rendafamiliar=rendafamiliar)
     session.add(aluno)
     session.commit()
-    mensagem = "cadastro efetuado com sucesso"
-    return render_template('index.html',msgbanco=mensagem)
+    mensagem2 = "cadastro efetuado com sucesso"
+    return render_template('novoaluno.html',mensagem2=mensagem2)
    
 app.run(debug=True)
